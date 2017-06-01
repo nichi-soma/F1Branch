6bb30f333484a68f745339097a5b96a1d42ba306
nichiuser <nichiuser@NIS-PC088>
2017-06-01 18:03:29 +0530

    Initial Commit

---
+++ src/SomaConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Displaying Tokyo Standard Time
+            //Console.WriteLine(" Current time in Tokyo is : " + JstNow().ToString());
+            
+
+            // Random sample 
+            Random cRandom = new System.Random();            
+            var randomValue = cRandom.Next(999999).ToString();
+            Console.WriteLine("Random Value : " + randomValue);
+
+            Console.Read();
+        }
+        private static DateTime JstNow()
+        {
+            var tokyoTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.Now.ToUniversalTime(), tokyoTimeZone);
+        }
+    }
+}
